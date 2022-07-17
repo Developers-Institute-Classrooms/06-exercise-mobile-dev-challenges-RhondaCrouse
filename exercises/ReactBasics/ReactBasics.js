045d6b26d867f8fdf4bf15dcf7fef4e51e7b35d8
RhondaCrouse <81496646+RhondaCrouse@users.noreply.github.com>
2022-07-17 21:12:51 +1200

    have finished exercise 2

--- exercises/ReactBasics/ReactBasics.js
+++ exercises/ReactBasics/ReactBasics.js
@@ -1,20 +1,44 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import { useState, useEffect } from "react";
-import { commonStyle, fontSize, color } from "../../commonStyles";
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { commonStyle, fontSize, color } from '../../commonStyles';
+
+const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
 
 export default function ReactBasics() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [user, setUser] = useState({ name: "", age: "" });
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [user, setUser] = useState({ name: '', age: '' });
+
+  const prevName = usePrevious[user.name];
+  const prevAge = usePrevious[user.age];
 
   useEffect(() => {
-    console.log("name state was updated");
+    console.log('name state was updated');
   }, [name]);
 
-  console.log("age state was updated");
-  console.log("age OR name state was updated");
-  console.log("user state was updated, but not necessarily changed ;)");
-  console.log("user state was CHANGED (bonus point)");
+  useEffect(() => {
+    console.log('age state was updated');
+  }, [age]);
+
+  useEffect(() => {
+    console.log('age OR name state was updated');
+  }, [name, age]);
+
+  useEffect(() => {
+    console.log('user state was updated, but not necessarily changed ;)');
+  }, [user]);
+
+  useEffect(() => {
+    if (prevName !== name || prevAge !== age) {
+      console.log('user state was CHANGED (bonus point)');
+    }
+  }, [user]);
 
   return (
     <View style={commonStyle.container}>
@@ -46,10 +70,10 @@ export default function ReactBasics() {
 const styles = StyleSheet.create({
   inputField: {
     fontSize: fontSize.small,
-    height: "2rem",
-    padding: "0.4rem",
-    borderRadius: "0.2rem",
-    width: "100%",
+    height: '2rem',
+    padding: '0.4rem',
+    borderRadius: '0.2rem',
+    width: '100%',
     color: color.black,
     backgroundColor: color.white,
   },
