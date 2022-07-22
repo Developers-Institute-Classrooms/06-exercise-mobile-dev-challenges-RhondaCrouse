a068d2ffb3440e94fa76c01eaf5de8d8f8ef9c23
RhondaCrouse <81496646+RhondaCrouse@users.noreply.github.com>
2022-07-22 21:51:35 +1200

    have finished the kata testing exercise

--- exercises/Kata/Kata.test.js
+++ exercises/Kata/Kata.test.js
@@ -1,57 +1,136 @@
-import { render, screen, fireEvent } from "@testing-library/react-native";
-import Kata from "./Kata";
+import { render, screen, fireEvent } from '@testing-library/react-native';
+import Kata from './Kata';
 
-describe("< Kata />", () => {
-  it("should contain a username and password input field", () => {
+describe('< Kata />', () => {
+  it('should contain a username and password input field', () => {
     render(<Kata />);
 
-    const usernameInput = screen.getByPlaceholderText("Username");
-    const passwordInput = screen.getByPlaceholderText("Password");
+    const usernameInput = screen.getByPlaceholderText('Username');
+    const passwordInput = screen.getByPlaceholderText('Password');
 
     expect(usernameInput).toBeTruthy();
     expect(passwordInput).toBeTruthy();
   });
 
-  it("should contain a create user button", () => {
+  it('should contain a create user button', () => {
     render(<Kata />);
 
-    const btn = screen.getByText("Create User");
+    const btn = screen.getByText('Create User');
     expect(btn).toBeTruthy();
   });
 
-  it("should create user once the username and password are filled out", () => {
+  it('should create user once the username and password are filled out', () => {
     const mockFn = jest.fn();
-    const testUsername = "test-user";
-    const testPassword = "AbcD123!@#";
+    const testUsername = 'test-user';
+    const testPassword = 'AbcD123!@#';
     render(<Kata createUser={mockFn} />);
 
-    const usernameInput = screen.getByPlaceholderText("Username");
-    const passwordInput = screen.getByPlaceholderText("Password");
+    const usernameInput = screen.getByPlaceholderText('Username');
+    const passwordInput = screen.getByPlaceholderText('Password');
 
     fireEvent.changeText(usernameInput, testUsername);
     fireEvent.changeText(passwordInput, testPassword);
 
-    const btn = screen.getByText("Create User");
+    const btn = screen.getByText('Create User');
 
     fireEvent.press(btn);
 
-    // TODO: Expect the mock function to have been called with the appropriate arguments.
-    // TODO: Expect the text "Password is strong enough!" to be shown.
+    expect(mockFn).toHaveBeemCalledWith({
+      username: testUsername,
+      password: testPassword,
+    });
+
+    const message = screen.getByText('Password is strong enough!');
+    expect(message).toBeTruthy();
   });
 
-  it.todo(
-    "should not create user if the password does not contain at least 2 capital letters"
-  );
+  it('should not create user if the password does not contain at least 2 capital letters', () => {
+    const mockFn = jest.fn();
+    const testUsername = 'test-user';
+    const testPassword = 'Abc123!@#';
+    render(<Kata createUser={mockFn} />);
 
-  it.todo(
-    "should not create user if the password does not contain at least 1 number"
-  );
+    const usernameInput = screen.getByPlaceholderText('Username');
+    const passwordInput = screen.getByPlaceholderText('Password');
 
-  it.todo(
-    "should not create user if the password is not longer than 8 characters"
-  );
+    fireEvent.changeText(usernameInput, testUsername);
+    fireEvent.changeText(passwordInput, testPassword);
 
-  it.todo(
-    "should not create user if the username is empty but the password is there"
-  );
+    const btn = screen.getByText('Create User');
+
+    fireEvent.press(btn);
+
+    expect(mockFn).not.toHaveBeenCalled();
+
+    const message = screen.getByText(
+      'Password should contain at least 8 characters, 2 uppercase letters and 1 number!'
+    );
+    expect(message).toBeTruthy();
+  });
+
+  it('should not create user if the password does not contain at least 1 number', () => {
+    const mockFn = jest.fn();
+    const testUsername = 'test-user';
+    const testPassword = 'AbcD123';
+    render(<Kata createUser={mockFn} />);
+
+    const usernameInput = screen.getByPlaceholderText('Username');
+    const passwordInput = screen.getByPlaceholderText('Password');
+
+    fireEvent.changeText(usernameInput, testUsername);
+    fireEvent.changeText(passwordInput, testPassword);
+
+    const btn = screen.getByText('Create User');
+
+    fireEvent.press(btn);
+
+    expect(mockFn).not.toHaveBeenCalled();
+
+    const message = screen.getByText(
+      'Password should contain at least 8 characters, 2 uppercase letters and 1 number!'
+    );
+    expect(message).toBeTruthy();
+  });
+  it('should not create user if the password is not longer than 8 characters', () => {
+    const mockFn = jest.fn();
+    const testUsername = 'test-user';
+    const testPassword = 'AbcD';
+    render(<Kata createUser={mockFn} />);
+
+    const usernameInput = screen.getByPlaceholderText('Username');
+    const passwordInput = screen.getByPlaceholderText('Password');
+
+    fireEvent.changeText(usernameInput, testUsername);
+    fireEvent.changeText(passwordInput, testPassword);
+
+    const btn = screen.getByText('Create User');
+
+    fireEvent.press(btn);
+
+    expect(mockFn).not.toHaveBeenCalled();
+
+    const message = screen.getByText(
+      'Password should contain at least 8 characters, 2 uppercase letters and 1 number!'
+    );
+    expect(message).toBeTruthy();
+  });
+
+  it('should not create user if the username is empty but the password is there', () => {
+    const mockFn = jest.fn();
+    const testPassword = 'AbcD123!@#';
+    render(<Kata createUser={mockFn} />);
+
+    const passwordInput = screen.getByPlaceholderText('Password');
+
+    fireEvent.changeText(passwordInput, testPassword);
+
+    const btn = screen.getByText('Create User');
+
+    fireEvent.press(btn);
+
+    expect(mockFn).not.toHaveBeenCalled();
+
+    const message = screen.getByText('Username is required!');
+    expect(message).toBeTruthy();
+  });
 });
